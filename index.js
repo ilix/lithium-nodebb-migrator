a09529ccbe9d4e7ebeaa02f614cf4d07a55ac65f
ilix <src@ilix.se>
2016-10-24 15:05:27 +0200

    Added user service, mongo adapter. Finished rudimentary user import. It needs to be extended with more fields.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const env = require('dotenv')
 const bootstrapper = require('./bootstrapper')
 const oracle = require('./lib/adapters/oracle')
 
+const userService = require('./lib/services/user')
+
 env.config()
 bootstrapper.checkConfiguration()
 
@@ -12,9 +14,12 @@ const command = process.argv[2]
 if (command === 'users') {
   console.log('Import users')
 
-  oracle.getLithiumAccounts(100)
+  oracle.getLithiumAccounts(1)
     .then(accounts => {
-      console.log('Got lithium accounts', accounts)
+      return userService.insert(accounts[0])
+    })
+    .then(result => {
+      console.log('insert results', result)
     })
     .catch(error => {
       console.error(error)
