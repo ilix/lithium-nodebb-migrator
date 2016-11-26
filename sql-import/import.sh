afa0776287361778de75d895f35cbdba54647605
ilix <src@ilix.se>
2016-11-26 10:03:07 +0100

    Updated sql import script, root pw and db name are now env vars.

--- sql-import/import.sh
+++ sql-import/import.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+# Wait for the mysql container
 echo "Waiting a moment to give mysql time to come online..."
 sleep 15
 
-echo "CREATE DATABASE IF NOT EXISTS lithium;" > /init.sql
+# Create database and set max_allowed_packet
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > /init.sql
 echo "SET GLOBAL max_allowed_packet=1073741824;" >> /init.sql
-mysql --host=lnm-mysql --user=root --password=meow < /init.sql 
+mysql --host=lnm-mysql --user=root --password=$MYSQL_ROOT_PASSWORD < /init.sql 
 
+# Run through the scripts
 for file in /data/*.sql
 do
   echo "Run $file"
-  mysql --host=lnm-mysql --user=root --password=meow --database=lithium < "$file"
+  mysql --host=lnm-mysql --user=root --password=$MYSQL_ROOT_PASSWORD --database=$MYSQL_DATABASE < "$file"
 done
 
+# All done
+echo
 echo "Lithium data import complete. Container will stop."
