7626efe5740c2203efcfa43aaed0125ab529e0fa
ilix <src@ilix.se>
2016-11-29 15:53:27 +0100

    Encoding fix for import

--- sql-import/import.sh
+++ sql-import/import.sh
@@ -2,18 +2,18 @@
 
 # Wait for the mysql container
 echo "Waiting a moment to give mysql time to come online..."
-sleep 30
+sleep 20
 
 # Create database and set max_allowed_packet
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > /init.sql
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET = 'utf8' DEFAULT COLLATE 'utf8_swedish_ci';" > /init.sql
 echo "SET GLOBAL max_allowed_packet=1073741824;" >> /init.sql
-mysql --host=lnm-mysql --user=root --password=$MYSQL_ROOT_PASSWORD < /init.sql 
+mysql --host=lnm-mysql --user=root --password=$MYSQL_ROOT_PASSWORD --default-character-set=utf8 < /init.sql 
 
 # Run through the scripts
 for file in /data/*.sql
 do
   echo "Run $file"
-  mysql --host=lnm-mysql --user=root --password=$MYSQL_ROOT_PASSWORD --database=$MYSQL_DATABASE < "$file"
+  mysql --host=lnm-mysql --user=root --password=$MYSQL_ROOT_PASSWORD --default-character-set=utf8 --database=$MYSQL_DATABASE < "$file"
 done
 
 # All done
