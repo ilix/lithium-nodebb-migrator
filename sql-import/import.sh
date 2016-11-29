bea54122f3b5d51054cbc9873835853d7b03c858
ilix <src@ilix.se>
2016-11-29 22:49:56 +0100

    changed mysql encoding to utf8mb4

--- sql-import/import.sh
+++ sql-import/import.sh
@@ -5,15 +5,15 @@ echo "Waiting a moment to give mysql time to come online..."
 sleep 20
 
 # Create database and set max_allowed_packet
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET = 'utf8' DEFAULT COLLATE 'utf8_swedish_ci';" > /init.sql
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET = 'utf8mb4' DEFAULT COLLATE 'utf8mb4_swedish_ci';" > /init.sql
 echo "SET GLOBAL max_allowed_packet=1073741824;" >> /init.sql
-mysql --host=lnm-mysql --user=root --password=$MYSQL_ROOT_PASSWORD --default-character-set=utf8 < /init.sql 
+mysql --host=lnm-mysql --user=root --password=$MYSQL_ROOT_PASSWORD --default-character-set=utf8mb4 < /init.sql 
 
 # Run through the scripts
 for file in /data/*.sql
 do
   echo "Run $file"
-  mysql --host=lnm-mysql --user=root --password=$MYSQL_ROOT_PASSWORD --default-character-set=utf8 --database=$MYSQL_DATABASE < "$file"
+  mysql --host=lnm-mysql --user=root --password=$MYSQL_ROOT_PASSWORD --default-character-set=utf8mb4 --database=$MYSQL_DATABASE < "$file"
 done
 
 # All done
