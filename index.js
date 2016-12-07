2087616d300e7cb8e8a8ff8275ede6317bb89628
ilix <src@ilix.se>
2016-12-07 08:44:20 +0100

    Refactored the command selector.

--- index.js
+++ index.js
@@ -11,94 +11,43 @@ const replyService = require('./lib/services/reply')
 const topicService = require('./lib/services/topic')
 const categoryService = require('./lib/services/category')
 
-const command = process.argv[2]
+let exitCode = 0
+const _command = process.argv[2]
 
-if (!command) {
+if (!_command) {
   logger.error('No command specified.')
   process.exit(1)
 }
 
-switch (command) {
-  case 'clean':
-    app.clean()
-      .then(() => {
-        process.exit(0)
-      })
-      .catch(error => {
-        console.error('clean error', error)
-      })
-    break
-  case 'init':
-    app.init()
-      .then(() => {
-        console.log('nodebb-plugin-write-api should be activated now. Restart nodebb.')
-        process.exit(0)
-      })
-      .catch(error => {
-        console.error(error)
-      })
-    break
-  case 'jsonUsers':
-    app.jsonUsers()
-      .then(() => {
-        console.log('json users imported')
-        process.exit(0)
-      })
-    break
-  case 'users':
-    userService.process()
-    .then(() => {
-      console.log('User import complete.')
-      process.exit(0)
-    })
-    .catch(error => {
-      logger.error('sorry, something went wrong')
-      console.log(error)
-      process.exit(1)
-    })
-    break
-  case 'initCategories':
-    categoryService.init()
-    .then(() => {
-      console.log('initial categories created')
-      process.exit(0)
-    })
-    .catch(error => {
-      logger.error('sorry, something went wrong')
-      console.log(error)
-      process.exit(1)
-    })
-    break
-  case 'nodes':
-    categoryService.process()
-      .then(() => {
-        console.log('nodes imported')
-      })
-      .catch(error => {
-        logger.error('sorry, something went wrong')
-        console.log(error)
-      })
-      .then(() => {
-        process.exit(0)
-      })
-    break
-  case 'topics':
-    topicService.process()
-      .then(() => {
-        console.log('Finished importing topics')
-        process.exit(0)
-      })
-      .catch(error => {
-        console.error('topics import error', error)
-      })
-    break
-  case 'replies':
-    replyService.process()
-      .then(() => {
-        console.log('Finished importing topic replies')
-        process.exit(0)
-      })
-    break
-  default:
-    logger.error(`Command "${command}" is not declared.`)
+const r = () => {
+  switch (_command) {
+    case 'clean':
+      return app.clean()
+    case 'init':
+      return app.init()
+    case 'jsonUsers':
+      return app.jsonUsers()
+    case 'users':
+      return userService.process()
+    case 'initCategories':
+      return categoryService.init()
+    case 'nodes':
+      return categoryService.process()
+    case 'topics':
+      return topicService.process()
+    case 'replies':
+      return replyService.process()
+    default:
+      return Promise.reject((`Command "${_command}" is not declared.`))
+  }
 }
+
+r()
+  .then(() => {
+    console.log(`Command '${_command}' completed!`)
+  })
+  .catch(error => {
+    exitCode = 1
+    console.error('Error:', error)
+  })
+  .then(() => process.exit(exitCode))
