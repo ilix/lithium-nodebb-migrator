3e45a5317367acf7ed8397569d8bedb74c2e3516
ilix <src@ilix.se>
2016-12-16 11:50:45 +0100

    Cleanup

--- test/unit/helpers/string.spec.js
+++ test/unit/helpers/string.spec.js
@@ -10,7 +10,6 @@ chai.use(require('sinon-chai'))
 describe('helpers/string', () => {
   describe('parseLiUserTag', () => {
     it('works', () => {
-      console.log('ssss')
       expect(helper.parseLiUserTag('<li-user uid="27" login="Akilles"></li-user>')).to.eql('@Akilles')
       expect(helper.parseLiUserTag('Hello! <li-user uid="17" login="Tortoise"></li-user> Bla. bla. bla.')).to.eql('Hello! @Tortoise Bla. bla. bla.')
       expect(helper.parseLiUserTag('Hello <li-user uid="17" login="Tortoise"></li-user> and <li-user uid="27" login="Akilles"></li-user> - This sentence is false')).to.eql('Hello @Tortoise and @Akilles - This sentence is false')
